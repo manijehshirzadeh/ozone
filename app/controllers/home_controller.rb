f9e034101fdd676f9499c80ef46ab6142b38992d
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-01-03 22:29:18 +1100

    added dynamic background color to the jumbotron, based on the air quality value

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     require "net/http"
     require "json"
     @url =
-      "https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=&distance=25&API_KEY=0126133B-69D1-416C-9A68-6262E4798470"
+      "https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=25&API_KEY=0126133B-69D1-416C-9A68-6262E4798470"
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @output = JSON.parse(@response)
@@ -13,6 +13,17 @@ class HomeController < ApplicationController
       @final_output = "Error"
     else
       @final_output = @output[0]["AQI"]
+    end
+
+    if @final_output == "Error"
+      @api_color = "gray"
+    else
+      @api_color = "green" if @final_output <= 50
+      @api_color = "yellow" if @final_output >= 50 && @final_output <= 100
+      @api_color = "orange" if @final_output >= 101 && @final_output <= 150
+      @api_color = "red" if @final_output >= 151 && @final_output <= 200
+      @api_color = "purple" if @final_output >= 201 && @final_output <= 300
+      @api_color = "maroon" if @final_output >= 301 && @final_output <= 500
     end
   end
 end
