1e2859315df31bba9514b05220f971d2707064b0
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-01-03 21:37:33 +1100

    Added the api call into the controller and show the response in the html view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
   def index
+    require "net/http"
+    require "json"
+    @url =
+      "https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=&distance=25&API_KEY=0126133B-69D1-416C-9A68-6262E4798470"
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @output = JSON.parse(@response)
+
+    #check for empty response
+    if (@output.empty? || !@output)
+      @final_output = "Error"
+    else
+      @final_output = @output[0]["AQI"]
+    end
   end
 end
