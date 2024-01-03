e7898a41959b42e772aa29a5905218e62bec3173
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-01-03 22:41:34 +1100

    Added a dynamic description

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,12 +18,36 @@ class HomeController < ApplicationController
     if @final_output == "Error"
       @api_color = "gray"
     else
-      @api_color = "green" if @final_output <= 50
-      @api_color = "yellow" if @final_output >= 50 && @final_output <= 100
-      @api_color = "orange" if @final_output >= 101 && @final_output <= 150
-      @api_color = "red" if @final_output >= 151 && @final_output <= 200
-      @api_color = "purple" if @final_output >= 201 && @final_output <= 300
-      @api_color = "maroon" if @final_output >= 301 && @final_output <= 500
+      if @final_output <= 50
+        @api_description =
+          "Air quality is satisfactory, and air pollution poses little or no risk."
+        @api_color = "green"
+      end
+      if @final_output >= 50 && @final_output <= 100
+        @api_description =
+          "Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution."
+        @api_color = "yellow"
+      end
+      if @final_output >= 101 && @final_output <= 150
+        @api_description =
+          "Members of sensitive groups may experience health effects. The general public is less likely to be affected."
+        @api_color = "orange"
+      end
+      if @final_output >= 151 && @final_output <= 200
+        @api_description =
+          "Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects."
+        @api_color = "red"
+      end
+      if @final_output >= 201 && @final_output <= 300
+        @api_description =
+          "Health alert: The risk of health effects is increased for everyone."
+        @api_color = "purple"
+      end
+      if @final_output >= 301 && @final_output <= 500
+        @api_description =
+          "Health warning of emergency conditions: everyone is more likely to be affected."
+        @api_color = "maroon"
+      end
     end
   end
 end
